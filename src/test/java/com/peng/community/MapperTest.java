5bb999eefd628ef5f852b5e0202ccc1551686d64
BigDangJia <786865925@qq.com>
2020-02-15 07:43:20 +0800

    首次进行提交

--- src/test/java/com/peng/community/MapperTest.java
+++ src/test/java/com/peng/community/MapperTest.java
@@ -1,12 +1,15 @@
 package com.peng.community;
 
 import com.peng.community.dao.DiscussPostMapper;
+import com.peng.community.dao.LoginTicketMapper;
 import com.peng.community.dao.UserMapper;
 import com.peng.community.entity.DiscussPost;
+import com.peng.community.entity.LoginTicket;
 import com.peng.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -24,6 +27,9 @@ public class MapperTest {
     //自动注入用户的mapper
     @Autowired
     private UserMapper userMapper;
+    //用户登陆的Mapper
+    @Autowired
+    private LoginTicketMapper ticketMapper;
 
     //自动注入DiscussPostMapper
     @Autowired
@@ -79,5 +85,28 @@ public class MapperTest {
         System.out.println(rows);
     }
 
+    //来进行测试登录模块的mapper
+    @Test
+    public void testInsetLoginTicket(){
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setId(10);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis()+1000 * 60 * 10));
+        int i = ticketMapper.insetLoginTicket(loginTicket);
+        System.out.println(i);
+    }
+    //根据登陆的令牌来进行查寻登陆的状况
+    @Test
+    public void testSelectTicket(){
+        LoginTicket loginTicket = ticketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+    }
 
+    //根据登陆的令牌来修改登录i的状态
+    @Test
+    public void updateTicket(){
+        int abc = ticketMapper.updateStatus("abc",1);
+        System.out.println(abc);
+    }
 }
